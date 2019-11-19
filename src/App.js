6d03793a41fa9ed684d77fd48725e7ac22085237
Alison Ludick <alison.ludick@gmail.com>
2019-11-19 22:02:03 +0200

    added new files, deleted old files, added image

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import Photos from './Photos'; 
 import "./App.css";
+import axios from "axios";
+import MediaContainer from "./MediaContainer";
+import styled from "styled-components";
 
-const photosApi = 'https://lambda-github-api-server.herokuapp.com/'; 
+const StyledApp = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-content: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
 
 function App() {
-  const [nasaData, setNasaData] = useState({});
+  const [mediaURL, setMediaURL] = useState();
+  const [description, setDescription] = useState();
+  const [title, setTitle] = useState();
+  const [mediaType, setMediaType] = useState();
+  const [date, setDate] = useState("");
 
   useEffect(() => {
-    axios.get(photosApi)
-      .then(response => {
-        setNasaData(response.data);
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=fAMua8JCgSmrRJO7mL9UOuJ3xH7fNdQUF4X8evVV&date=${date}`
+      )
+      .then(res => {
+        setMediaURL(res.data.url);
+        setTitle(res.data.title);
+        setDate(res.data.date);
+        setDescription(res.data.explanation);
+        setMediaType(res.data.media_type);
       })
       .catch(error => {
-        console.log("PROBLEMS!")
+        console.log(error);
       });
-  }, []);
+  }, [mediaURL, date]);
 
   return (
-    <div className="App">
-      <Photos 
-        Title={nasaData.title}
-        Image={nasaData.url}
-        Story={nasaData.explanation}
-      />
-    </div>
+    <>
+      <StyledApp className="App container">
+        <div className="heading">
+          <h2>NASA Photo of the Day</h2>
+        </div>
+
+        <MediaContainer
+          date={date}
+          mediaType={mediaType}
+          title={title}
+          description={description}
+          setDate={setDate}
+          mediaURL={mediaURL}
+        />
+      </StyledApp>
+    </>
   );
-  
 }
 
 export default App;
